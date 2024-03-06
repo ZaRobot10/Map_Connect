e43eaf60a380799b90df61d56629421354e3ba67
Ekankaar Khera <ekankaar@Ekankaars-MacBook-Air.local>
2024-03-06 17:48:14 +0530

    map changes

--- src/Maps.js
+++ src/Maps.js
@@ -1,13 +1,15 @@
-import {React, useEffect} from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet';
+import React, { useEffect } from 'react';
+
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-routing-machine';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import L from 'leaflet';
 
-
 const icon = L.icon({
-    iconUrl: "./placeholder.png",
-    iconSize: [48, 38],
-  });
+  iconUrl: "./placeholder.png",
+  iconSize: [48, 38],
+});
 
 const position = [28.6139, 77.2090];
 
@@ -30,10 +32,15 @@ function ResetCenterView(props) {
   return null;
 }
 
-
 export default function Maps(props) {
   const { selectPosition } = props;
   const locationSelection = [selectPosition?.lat, selectPosition?.lon];
+
+  const handleMapClick = (e) => {
+    console.log("Latitude:", e.latlng.lat);
+    console.log("Longitude:", e.latlng.lng);
+  };
+
   return (
     <MapContainer center={position} zoom={13} style={{ width: '100%', height: '100%' }}>
       {/* OpenStreetMap TileLayer */}
@@ -50,7 +57,7 @@ export default function Maps(props) {
         subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
       />
 
-{selectPosition && (
+      {selectPosition && (
         <Marker position={locationSelection} icon={icon}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
@@ -58,6 +65,52 @@ export default function Maps(props) {
         </Marker>
       )}
       <ResetCenterView selectPosition={selectPosition} />
+      {/* Listen for map click event */}
+      <UseMapClick />
     </MapContainer>
   );
+}
+
+let firstMarker, secondMarker;
+
+function UseMapClick() {
+  const map = useMapEvent('click', (e) => {
+    const { latlng } = e;
+    if (!firstMarker) {
+      firstMarker = L.marker(latlng, { icon }).addTo(map);
+    } else if (!secondMarker) {
+      secondMarker = L.marker(latlng, { icon }).addTo(map);
+      // Routing between the first two markers
+      L.Routing.control({
+        waypoints: [
+          L.latLng(firstMarker.getLatLng()),
+          L.latLng(secondMarker.getLatLng())
+        ],
+        lineOptions: {
+          styles: [{ color: '#00ff00', weight: 5 }]
+        },
+        createMarker: function (i, waypoint, n) {
+          // The icons for start and end markers
+          if (i === 0) {
+            return L.marker(waypoint.latLng, {
+              icon: L.icon({
+                iconUrl: './placeholder.png',
+                iconSize: [48, 38]
+              })
+            });
+          } else if (i === n - 1) {
+            return L.marker(waypoint.latLng, {
+              icon: L.icon({
+                iconUrl: './placeholder.png',
+                iconSize: [48, 38]
+              })
+            });
+          }
+          return null;
+        }
+      }).addTo(map);
+    }
+  });
+
+  return null;
 }
